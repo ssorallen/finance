0140ba6e15c8aa984a7e6fe7f5112c11da436d0e
Ross Allen <rossgit@gmail.com>
2018-05-12 12:53:32 +0800

    Make all type immutable

--- src/types.js
+++ src/types.js
@@ -1,99 +1,105 @@
 /* @flow */
 
-type AddSymbolAction = {
-  symbol: string,
-  type: 'ADD_SYMBOL',
-};
+export type Chart = Array<{|
+  +change: number,
+  +changeOverTime: number,
+  +changePercent: number,
+  +close: number,
+  +date: string,
+  +high: number,
+  +label: string,
+  +low: number,
+  +open: number,
+  +unadjustedVolume: number,
+  +volume: number,
+  +vwap: number,
+|}>;
 
-export type Chart = Array<{
-  change: number,
-  changeOverTime: number,
-  changePercent: number,
-  close: number,
-  date: string,
-  high: number,
-  label: string,
-  low: number,
-  open: number,
-  unadjustedVolume: number,
-  volume: number,
-  vwap: number,
-}>;
+export type Transaction = {|
+  +cashValue: ?number,
+  +commission: number,
+  +date: ?string,
+  +id: number,
+  +notes: ?string,
+  +price: number,
+  +shares: number,
+  +symbol: string,
+  +type: 'Buy' | 'Sell',
+|};
 
-export type Transaction = {
-  cashValue: ?number,
-  commission: number,
-  date: ?string,
-  id: number,
-  notes: ?string,
-  price: number,
-  shares: number,
-  symbol: string,
-  type: 'Buy' | 'Sell',
-};
+type AddSymbolAction = {|
+  +symbol: string,
+  +type: 'ADD_SYMBOL',
+|};
 
-type AddTransactionAction = {
-  transaction: Transaction,
-  type: 'ADD_TRANSACTION',
-};
+type AddTransactionAction = {|
+  +transaction: Transaction,
+  +type: 'ADD_TRANSACTION',
+|};
 
-type AddTransactionsAction = {
-  transactions: Array<Transaction>,
-  type: 'ADD_TRANSACTIONS',
-};
+type AddTransactionsAction = {|
+  +transactions: Array<Transaction>,
+  +type: 'ADD_TRANSACTIONS',
+|};
 
-type DeletePortfolioAction = {
-  type: 'DELETE_PORTFOLIO',
-};
+type ChangePageSizeAction = {|
+  +pageSize: number,
+  +type: 'CHANGE_PAGE_SIZE',
+|};
 
-type DeleteSymbolsAction = {
-  symbols: Array<string>,
-  type: 'DELETE_SYMBOLS',
-};
+type DeletePortfolioAction = {|
+  +type: 'DELETE_PORTFOLIO',
+|};
 
-type DeleteTransactionsAction = {
-  transactions: Array<Transaction>,
-  type: 'DELETE_TRANSACTIONS',
-};
+type DeleteSymbolsAction = {|
+  +symbols: Array<string>,
+  +type: 'DELETE_SYMBOLS',
+|};
 
-type DownloadPortfolioAction = {
-  type: 'DOWNLOAD_PORTFOLIO',
-};
+type DeleteTransactionsAction = {|
+  +transactions: Array<Transaction>,
+  +type: 'DELETE_TRANSACTIONS',
+|};
 
-type FetchChartFailureAction = {
-  error: TypeError,
-  type: 'FETCH_CHART_FAILURE',
-};
+type DownloadPortfolioAction = {|
+  +type: 'DOWNLOAD_PORTFOLIO',
+|};
 
-type FetchChartRequestAction = {
-  error: TypeError,
-  type: 'FETCH_CHART_REQUEST',
-};
+type FetchChartFailureAction = {|
+  +error: TypeError,
+  +type: 'FETCH_CHART_FAILURE',
+|};
 
-type FetchChartSuccessAction = {
-  chartData: Chart,
-  symbol: string,
-  type: 'FETCH_CHART_SUCCESS',
-};
+type FetchChartRequestAction = {|
+  +error: TypeError,
+  +type: 'FETCH_CHART_REQUEST',
+|};
 
-type FetchQuotesFailureAction = {
-  error: TypeError,
-  type: 'FETCH_QUOTES_FAILURE',
-};
+type FetchChartSuccessAction = {|
+  +chartData: Chart,
+  +symbol: string,
+  +type: 'FETCH_CHART_SUCCESS',
+|};
 
-type FetchQuotesRequestAction = {
-  type: 'FETCH_QUOTES_REQUEST',
-};
+type FetchQuotesFailureAction = {|
+  +error: TypeError,
+  +type: 'FETCH_QUOTES_FAILURE',
+|};
 
-type FetchQuotesSuccessAction = {
-  quotes: { [symbol: string]: Object },
-  type: 'FETCH_QUOTES_SUCCESS',
-};
+type FetchQuotesRequestAction = {|
+  +type: 'FETCH_QUOTES_REQUEST',
+|};
+
+type FetchQuotesSuccessAction = {|
+  +quotes: { [symbol: string]: Object },
+  +type: 'FETCH_QUOTES_SUCCESS',
+|};
 
 export type Action =
   | AddSymbolAction
   | AddTransactionAction
   | AddTransactionsAction
+  | ChangePageSizeAction
   | DeletePortfolioAction
   | DeleteSymbolsAction
   | DeleteTransactionsAction
@@ -105,36 +111,39 @@ export type Action =
   | FetchQuotesRequestAction
   | FetchQuotesSuccessAction;
 
-export type Quote = {
-  avgTotalVolume: number,
-  change: number,
-  changePercent: number,
-  close: number,
-  companyName: string,
-  high: number,
-  latestPrice: number,
-  latestVolume: number,
-  low: number,
-  marketCap: number,
-  open: number,
-  peRatio: ?number,
-  previousClose: number,
-  price: number,
-  symbol: string,
-  week52High: number,
-  week52Low: number,
-};
+export type Quote = {|
+  +avgTotalVolume: number,
+  +change: number,
+  +changePercent: number,
+  +close: number,
+  +companyName: string,
+  +high: number,
+  +latestPrice: number,
+  +latestVolume: number,
+  +low: number,
+  +marketCap: number,
+  +open: number,
+  +peRatio: ?number,
+  +previousClose: number,
+  +price: number,
+  +symbol: string,
+  +week52High: number,
+  +week52Low: number,
+|};
 
-export type AppState = {
-  charts: { [symbol: string]: Chart },
-  fetchErrorMessage: ?string,
-  isFetchingCount: number,
-  nextTransactionId: number,
-  quotes: { [symbol: string]: Quote },
-  symbols: Array<string>,
-  transactions: Array<Transaction>,
-  updatedAt: ?number,
-};
+export type AppState = {|
+  +appSettings: {
+    +pageSize: number,
+  },
+  +charts: { [symbol: string]: Chart },
+  +fetchErrorMessage: ?string,
+  +isFetchingCount: number,
+  +nextTransactionId: number,
+  +quotes: { [symbol: string]: Quote },
+  +symbols: Array<string>,
+  +transactions: Array<Transaction>,
+  +updatedAt: ?number,
+|};
 
 export type GetState = () => AppState;
 type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
