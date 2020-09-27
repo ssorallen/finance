a0db9a279d1f20aa70adb206a9f3490e51d735e6
Ross Allen <rossgit@gmail.com>
2020-09-26 22:50:06 -0500

    Updates

---
+++ precache-manifest.d3561d61cfce7ee503d2dc4ee29e03c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d590ec007d296cad1b640dc8461b040",
+    "url": "/finance/index.html"
+  },
+  {
+    "revision": "2b5b3098d54d4f9b5955",
+    "url": "/finance/static/css/2.4b306f74.chunk.css"
+  },
+  {
+    "revision": "fdd9a7e09362381c148c",
+    "url": "/finance/static/css/main.5c315a89.chunk.css"
+  },
+  {
+    "revision": "2b5b3098d54d4f9b5955",
+    "url": "/finance/static/js/2.b4d4f245.chunk.js"
+  },
+  {
+    "revision": "2f97bfe5b7c1c72b23be28739730a643",
+    "url": "/finance/static/js/2.b4d4f245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd9a7e09362381c148c",
+    "url": "/finance/static/js/main.27e68efe.chunk.js"
+  },
+  {
+    "revision": "a058ced0c4077819c0b5",
+    "url": "/finance/static/js/runtime-main.1aeee481.js"
+  }
+]);
